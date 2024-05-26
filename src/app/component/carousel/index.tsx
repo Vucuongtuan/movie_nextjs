0151681238fd5884ad7706144bddc25e7af539a3
Vucuongtuan <vucuongtuan00@gmail.com>
2024-05-26 17:54:36 +0700

    asd

--- src/app/component/carousel/index.tsx
+++ src/app/component/carousel/index.tsx
@@ -53,7 +53,7 @@ export default function CarouselSize({ data }: { data: any }) {
                       </div>
                     </Link>
                   </CardContent>
-                  <div className="h-6 text-sm w-full flex justify-between pt-1 px-1 absolute top-0 right-0 dark:text-white font-[500] min-[200px]:max-md:text-[0.5rem]">
+                  <div className="h-6 text-sm w-full flex justify-between pt-1 px-1 absolute top-0 right-0 a dark:text-white font-[500] min-[200px]:max-md:text-[0.5rem]">
                     {item.episode_current.length > 8 ? (
                       <span className="bg-white dark:bg-[#1f1f1f] h-full rounded-full px-2">
                         {item.episode_current.slice(0, 8)}
